2f1ec339278f924cd85c976860450fba51c2143f
Najmus Sakib <92445715+najmusskb@users.noreply.github.com>
2022-06-03 10:24:22 +0600

    first commit

---
+++ array.js
@@ -0,0 +1,58 @@
+// variable declared----1
+
+var priyobook = humu;
+
+
+
+
+// array-----2
+var booklist = ['kajal', 'himu', 'uponnash', 'ognibina', 'lalsalu'];
+
+var ognibinaindex = booklist.indexOf('ognibina');
+
+booklist[1] = 'momotadi';
+
+booklist.push('small-book');
+
+booklist.pop();
+
+
+
+
+
+
+// condition------3
+
+
+if (booklist == 'himu') {
+    console.log('im himu');
+}
+else {
+    console.log('im not here');
+}
+
+
+
+
+
+
+// loop while loop----4
+
+var i = 0;
+
+while (i < 15) {
+    console.log(i);
+    console.log('loop looping looping ');
+    i++;
+}
+
+
+
+
+
+
+// for loop----5
+
+for (var i = 0; i < 15; i++) {
+    console.log(i);
+}
